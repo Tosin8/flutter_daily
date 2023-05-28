bc1715197455c410c775bc111b73b7d0e131020b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-28 23:28:23 +0100

    Update splash_screen.dart

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -12,7 +12,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     Future.delayed(
       const Duration(seconds: 3),

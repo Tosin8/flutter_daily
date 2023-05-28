bffcc68e9d8ab133a6ccd9dc42f3d887b7fac030
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-28 23:44:17 +0100

    Updating with SnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(),
-        home: const Snack_Bar());
+        home: const SnackBarDemo());
   }
 }
--- lib/snack_bar.dart
+++ lib/snack_bar.dart
@@ -1,16 +1,51 @@
+//
+
 import 'package:flutter/material.dart';
 
-class Snack_Bar extends StatelessWidget {
-  const Snack_Bar({super.key});
+void main() => runApp(const SnackBarDemo());
+
+class SnackBarDemo extends StatelessWidget {
+  const SnackBarDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SnackBar Example'),
+    return MaterialApp(
+      title: 'SnackBar Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SnackBar Demo'),
+          centerTitle: true,
+        ),
+        body: const SnackBarPage(),
       ),
-      body: const Center(
-          child: ElevatedButton(onPressed: (null), child: Text('Show Toast'))),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  const SnackBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Show SnackBar'),
+      ),
     );
   }
 }

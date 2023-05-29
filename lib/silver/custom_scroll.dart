81614145d56dc26ffe08284fbf3202ec035588f2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-29 17:29:43 +0100

    Create slivergrid

--- lib/silver/custom_scroll.dart
+++ lib/silver/custom_scroll.dart
@@ -9,10 +9,26 @@ class Custom_Scroll extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverList(
-              delegate: SliverChildListDelegate([
+              delegate: SliverChildListDelegate.fixed([
             Container(
               height: 200,
               color: Colors.amber,
+            ),
+            Container(
+              height: 200,
+              color: Colors.green,
+            ),
+            Container(
+              height: 200,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 200,
+              color: Colors.indigo,
             ),
           ]))
         ],
@@ -20,3 +36,20 @@ class Custom_Scroll extends StatelessWidget {
     );
   }
 }
+
+//
+// SliverList(
+//         delegate: SliverChildBuilderDelegate(
+//            childCount: 20,
+//             (context, index) => Container(
+//                height: 100,
+//                color: Colors.primaries[Random().nextInt(
+//                  Colors.primaries.length,
+//               )],
+//             ),
+//           ),
+//         )
+
+// Sliver list flavors: 
+// 1. SliverAnimatedList : which animates items added to or removed from a list. 
+// 2. 

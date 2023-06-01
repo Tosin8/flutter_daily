a52e4a54b6772430227a626ec3a226a50db99062
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-01 11:01:58 +0100

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -22,6 +22,20 @@ class Profile_Page extends StatelessWidget {
             backgroundImage: NetworkImage(
                 'https://images.unsplash.com/photo-1685432051879-31fa592be8a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'),
           ),
+          SizedBox(height: 10),
+          Column(
+            children: [
+              Text(
+                'Eren Jager',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              Text(
+                '@Eren Jager',
+                style:
+                    TextStyle(fontWeight: FontWeight.w400, color: Colors.grey),
+              )
+            ],
+          )
         ],
       ),
     );

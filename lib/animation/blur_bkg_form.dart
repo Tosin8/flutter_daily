b76b63062650aa394b1513346603f1bc402ddf55
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-06-17 21:34:11 +0100

    Update blur_bkg_form.dart

--- lib/animation/blur_bkg_form.dart
+++ lib/animation/blur_bkg_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Blur_Form extends StatelessWidget {
   const Blur_Form({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -27,3 +28,4 @@ class Blur_Form extends StatelessWidget {
     );
   }
 }
+
